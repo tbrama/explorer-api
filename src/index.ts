dbb5e6ecc791359f6518c47d0b2055ac14fe2216
tbrama <47262890+tbrama@users.noreply.github.com>
2025-02-19 12:05:41 +0700

    feat(add-folder): Validasi dan buat folder

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,64 @@ const app = new Elysia()
 		body: t.Object({
       parentDir: inFolder.parentDir,
       foldersName: inFolder.foldersName,
+  .post(
+    "/add-folder",
+    async ({ body }) => {
+      const regex = /^[a-zA-Z0-9\s]+$/;
+      if (!regex.test(body.foldersName))
+        return {
+          list: Array(),
+          msg: `Nama folder tidak boleh ada spesial karakter`,
+        };
+
+      const checkExist = await conn
+        .select()
+        .from(folders)
+        .where(
+          and(
+            eq(folders.foldersName, body.foldersName),
+            eq(folders.parentDir, body.parentDir)
+          )
+        );
+      if (checkExist.length) {
+        return { list: Array(), msg: `Folder ${body.foldersName} sudah ada` };
+      }
+
+      const resultFolder = await conn
+        .insert(folders)
+        .values(body)
+        .$returningId();
+      const resFdlr = resultFolder.map((val) => val.id)[0];
+
+      const resUnion = await conn.execute(
+        sql.raw(`WITH RECURSIVE hierarchy_paths AS (
+          SELECT id, folders_name, parentDir, CAST(folders_name AS VARCHAR(255)) AS path
+          FROM folders
+          WHERE parentDir = 0
+          UNION ALL
+          SELECT h.id, h.folders_name, h.parentDir,
+          CAST(CONCAT(hp.path, '/', h.folders_name) AS VARCHAR(255)) AS path
+          FROM folders h
+          INNER JOIN hierarchy_paths hp ON h.parentDir = hp.id
+          )
+          SELECT *
+          FROM hierarchy_paths`)
+      );
+
+      //@ts-ignore
+      const path = resUnion[0].find(({ id }) => id == resFdlr).path;
+
+      await mkdir(`explorer/${path}`, { recursive: true });
+
+      return { list: resUnion[0], msg: "" };
+    },
+    {
+      body: t.Object({
+        parentDir: inFolder.parentDir,
+        foldersName: inFolder.foldersName,
+      }),
+    }
+  )
     }
 		)}
   )
